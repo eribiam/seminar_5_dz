27e04db6346ec9a12bd5ff2821f3aa8cba5d06b3
dmitriy <boss.vede@mail.ru>
2022-08-25 19:17:25 +0700

    second exercise

--- Program.cs
+++ Program.cs
@@ -19,7 +19,19 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-
+// System.Console.WriteLine("Введите размер массива:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int [size];
+// int sum = 0;
+// System.Console.Write("[ ");
+// for (int i = 0; i < size; i++)
+// {
+//     array[i] = new Random().Next(100);
+//     System.Console.Write($"{array[i]} ");
+//     if (i == 0 || i % 2 == 0) sum += array[i];
+// }
+// System.Console.WriteLine("]");
+// System.Console.WriteLine($"Сумма всех элементов, стоящих на нечётных позициях = {sum}");
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
